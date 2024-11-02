54b7e23be12af123b1094829e80838b423705e18
Sumit524 <sumitchaurasiya971@gmail.com>
2024-11-02 16:28:38 +0530

    Login Code

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Header.css";
+import bgwoman from "../assets/bg-woman.jpeg";
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={bgwoman} alt="Background woman" className="header-bg" />
+            <div className="header-content">
+                <h1></h1>
+                <h3>Buy from us now!</h3>
+                <button>Start singing</button>
+            </div>
+           
+        </header>
+    )
+}
+
+export default Header;
